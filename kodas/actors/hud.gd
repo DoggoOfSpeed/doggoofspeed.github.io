7583975795a15a20f3998c912c96b583ab93852f
DoggoOfSpeed <doggoofspeed@gmail.com>
2023-05-24 21:27:01 +0300

    touch

--- kodas/actors/hud.gd
+++ kodas/actors/hud.gd
@@ -6,3 +6,13 @@ func _ready():
 
 func _process(_delta):
 	$fps.text = str(Engine.get_frames_per_second())
+
+
+func _on_LMB_button_down():
+	InputEventMouseButton.new()
+	get_tree().input_event()
+	pass # Replace with function body.
+
+
+func _on_RMB_button_down():
+	pass # Replace with function body.
--- kodas/actors/player.gd
+++ kodas/actors/player.gd
@@ -10,8 +10,8 @@ onready var hudtxt = $CanvasLayer/hud/Label
 onready var galva = $Camera
 onready var cast = $Camera/RayCast
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _physics_process(_delta):
 	galva.rotation_degrees.x = lokrot.x
@@ -24,8 +24,13 @@ func _input(event):
 		lokrot.x = clamp(lokrot.x, min_angle, max_angle)
 		
 func _process(_delta):
-	if Input.is_action_just_pressed("bad"):			
-		if cast.is_colliding():
+	if Input.is_action_just_pressed("bad"):
+		fbad()
+	if Input.is_action_just_pressed("good"):
+		fgood()
+
+func fbad():
+	if cast.is_colliding():
 			var target = cast.get_collider()
 			if target.is_in_group("sk"):
 				pts += 1
@@ -34,8 +39,8 @@ func _process(_delta):
 			target.queue_free()
 			hudtxt.text = str(pts);
 			
-	if Input.is_action_just_pressed("good"):
-		if cast.is_colliding():
+func fgood():
+	if cast.is_colliding():
 			var target = cast.get_collider()
 			if target.is_in_group("sl"):
 				pts += 1
@@ -43,3 +48,4 @@ func _process(_delta):
 				pts -= 1
 			target.queue_free()
 			hudtxt.text = str(pts);
+	
