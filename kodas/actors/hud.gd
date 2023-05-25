558bfaa2f174ff5e6d564783c42fb991acbadc62
DoggoOfSpeed <doggoofspeed@gmail.com>
2023-05-25 15:53:23 +0300

    tutorial + timer

--- kodas/actors/hud.gd
+++ kodas/actors/hud.gd
@@ -3,6 +3,8 @@ extends Control
 signal lmbpress
 signal rmbpress
 
+var sec = 185
+
 func _ready():
 	$Label.text = "0";
 	pass
@@ -16,4 +18,10 @@ func _on_LMB_button_down():
 
 func _on_RMB_button_down():
 	emit_signal("rmbpress")
+	pass # Replace with function body.
+
+
+func _on_Timer_timeout():
+	sec -= 1
+	$Label2.text = ("%02d:%02d" % [sec/60, sec%60])
 	pass # Replace with function body.
