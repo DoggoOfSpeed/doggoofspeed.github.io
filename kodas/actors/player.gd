e26e7a41ee3c216e54171105edf379251ea14ff6
DoggoOfSpeed <doggoofspeed@gmail.com>
2023-05-27 21:39:20 +0300

    fix touchscreen

--- kodas/actors/player.gd
+++ kodas/actors/player.gd
@@ -21,10 +21,8 @@ func _physics_process(_delta):
 
 func _input(event):
 	if Input.is_action_just_pressed("bad"):
-		capture_check()
 		fbad()
 	if Input.is_action_just_pressed("good"):
-		capture_check()
 		fgood()
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -35,6 +33,7 @@ func _input(event):
 		lokrot.x = clamp(lokrot.x, min_angle, max_angle)
 
 func fbad():
+	capture_check()
 	if cast.is_colliding():
 			var target = cast.get_collider()
 			if target.is_in_group("sk"):
@@ -44,6 +43,7 @@ func fbad():
 			target.queue_free()
 			
 func fgood():
+	capture_check()
 	if cast.is_colliding():
 			var target = cast.get_collider()
 			if target.is_in_group("sl"):
